d2d55de2ee7792f88d76c1caafa87988f5bf7fa3
GodofNades <collin05@gmail.com>
2023-12-17 23:17:03 -0600

    1234

--- UI Tweaks.js
+++ UI Tweaks.js
@@ -16,7 +16,7 @@
 (function () {
 	"use strict";
     console.log(`UIT Loaded ${new Date()}`);
-    console.log('unsafeWindow:', unsafeWindow);
+    console.log('unsafeWindow:', unsafeWindow)
     if (window.self !== window.top) {
         console.log('This script is running in an iframe!');
     } else {
