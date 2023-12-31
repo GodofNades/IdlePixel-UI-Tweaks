6000aecf4971e881bed7e355359ffbeddb81d8f7
GodofNades <collin05@gmail.com>
2023-12-31 11:43:21 -0600

    Initial Commit

---
+++ Slapchop/Slapchop - Gathering.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         IdlePixel SlapChop - Gathering Code
+// @namespace    godofnades.idlepixel
+// @version      0.1
+// @description  Split off of IdlePixel Slapchop for all Gathering Code.
+// @author       GodofNades
+// @license      MIT
+// @match        *://idle-pixel.com/login/play/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+
+    
+		function quickGather(bag, alt) {
+			let n = IdlePixelPlus.getVarOrDefault(bag, 0, "int");
+			singleOverride =
+				IdlePixelPlus.plugins.slapchop.getConfig("autoSingleEnabled");
+			if (alt || singleOverride) {
+				n--;
+			}
+			if (n > 0) {
+				IdlePixelPlus.sendMessage(
+					`OPEN_GATHERING_LOOT=${bag.replace("gathering_loot_bag_", "")}~${n}`
+				);
+			}
+		}
+
+		function initQuickGather() {
+			LOOT_BAGS.forEach((item) => {
+				$(`itembox[data-item="${item}"]`).on("contextmenu", (event) => {
+					if (IdlePixelPlus.plugins.slapchop.getConfig("quickGatherRightClickEnabled")) {
+						const primary = window.isPrimaryActionSlapchop(event);
+						const alt = window.isAltActionSlapchop(event);
+						if (primary || alt) {
+							window.quickGather(item, !primary);
+							event.stopPropagation();
+							event.preventDefault();
+							return false;
+						}
+					}
+					return true;
+				});
+			});
+		}
+    
+	window.initQuickGather = initQuickGather;
+	window.quickGather = quickGather;
+})();
