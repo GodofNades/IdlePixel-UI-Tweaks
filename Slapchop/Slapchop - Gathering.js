a0fafe53a65c53d164cd57d3749e8762ab507ff5
GodofNades <collin05@gmail.com>
2023-12-31 12:05:22 -0600

    Added the LOOT_BAGS const to gathering code

--- Slapchop/Slapchop - Gathering.js
+++ Slapchop/Slapchop - Gathering.js
@@ -12,39 +12,47 @@
 (function () {
 	"use strict";
 
-    
-		function quickGather(bag, alt) {
-			let n = IdlePixelPlus.getVarOrDefault(bag, 0, "int");
-			singleOverride =
-				IdlePixelPlus.plugins.slapchop.getConfig("autoSingleEnabled");
-			if (alt || singleOverride) {
-				n--;
-			}
-			if (n > 0) {
-				IdlePixelPlus.sendMessage(
-					`OPEN_GATHERING_LOOT=${bag.replace("gathering_loot_bag_", "")}~${n}`
-				);
-			}
-		}
+	const LOOT_BAGS = $(`itembox[data-item^="gathering_loot_bag_"]`)
+		.toArray()
+		.map((el) => el.getAttribute("data-item"));
 
-		function initQuickGather() {
-			LOOT_BAGS.forEach((item) => {
-				$(`itembox[data-item="${item}"]`).on("contextmenu", (event) => {
-					if (IdlePixelPlus.plugins.slapchop.getConfig("quickGatherRightClickEnabled")) {
-						const primary = window.isPrimaryActionSlapchop(event);
-						const alt = window.isAltActionSlapchop(event);
-						if (primary || alt) {
-							window.quickGather(item, !primary);
-							event.stopPropagation();
-							event.preventDefault();
-							return false;
-						}
-					}
-					return true;
-				});
-			});
+	function quickGather(bag, alt) {
+		let n = IdlePixelPlus.getVarOrDefault(bag, 0, "int");
+		singleOverride =
+			IdlePixelPlus.plugins.slapchop.getConfig("autoSingleEnabled");
+		if (alt || singleOverride) {
+			n--;
 		}
-    
+		if (n > 0) {
+			IdlePixelPlus.sendMessage(
+				`OPEN_GATHERING_LOOT=${bag.replace("gathering_loot_bag_", "")}~${n}`
+			);
+		}
+	}
+
+	function initQuickGather() {
+		LOOT_BAGS.forEach((item) => {
+			$(`itembox[data-item="${item}"]`).on("contextmenu", (event) => {
+				if (
+					IdlePixelPlus.plugins.slapchop.getConfig(
+						"quickGatherRightClickEnabled"
+					)
+				) {
+					const primary = window.isPrimaryActionSlapchop(event);
+					const alt = window.isAltActionSlapchop(event);
+					if (primary || alt) {
+						window.quickGather(item, !primary);
+						event.stopPropagation();
+						event.preventDefault();
+						return false;
+					}
+				}
+				return true;
+			});
+		});
+	}
+
 	window.initQuickGather = initQuickGather;
 	window.quickGather = quickGather;
+    window.LOOT_BAGS = LOOT_BAGS;
 })();
